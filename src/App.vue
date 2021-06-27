9e47fbb91f386322b7a8a420b47ffe4eea0e5adb
Sohyun Park <shpark5938@gmail.com>
2021-06-28 01:49:21 +0900

    ADD login, signup page

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vue-tempalte">
+    <!-- Navigation -->
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+        <div class="inner-block">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    
+    data: () => ({
+      //
+    }),
+  };
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="vue-tempalte">
+        <form>
+            <h3>Sign In</h3>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+            <br>
+            <div>
+                <button type="submit" class="btn btn-dark btn-lg btn-block">Sign In</button>
+            </div>
+            
+            <p class="forgot-password text-right">
+                Don't you have ID ? 
+                <router-link :to="{name: 'signup'}">sign up</router-link>
+            </p>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+<style src="../assets/css/login.css">
+</style>
